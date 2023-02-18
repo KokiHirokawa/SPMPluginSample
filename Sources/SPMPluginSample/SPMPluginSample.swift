c72fa38cef447cf2a55e1b82c525a4ed8c55c6db
KokiHirokawa <kitakitsune1110@gmail.com>
2023-02-18 21:33:54 +0900

    swift package init

---
+++ Sources/SPMPluginSample/SPMPluginSample.swift
@@ -0,0 +1,6 @@
+public struct SPMPluginSample {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SPMPluginSampleTests/SPMPluginSampleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SPMPluginSample
+
+final class SPMPluginSampleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SPMPluginSample().text, "Hello, World!")
+    }
+}
